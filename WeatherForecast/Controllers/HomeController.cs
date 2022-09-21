4e0d261cf3849f1cb1e7b38df80d41a0804211fc
Ahmad Safwat Shanta <assh38002@edu.ucl.dk>
2022-09-21 11:35:34 +0200

    Add project files.

---
+++ WeatherForecast/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            string URL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f1cee7f9e0458329352f11617b9d5bb2";
+            var result = client.GetFromJsonAsync<Root>(URL);
+            return View(result.Result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WeatherForecast/Controllers/WeatherForecast.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.Controllers
+{
+    public class WeatherForecast : Controller
+    {
+        public DateTime Date { get; private set; }
+        public int TemperatureC { get; private set; }
+        public string Summary { get; private set; }
+
+        [ApiController]
+        [Route("[controller]")]
+        public class WeatherForecastController : ControllerBase
+        {
+            private static readonly string[] Summaries = new[]
+            {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+             };
+
+            [HttpGet]
+            public IEnumerable<WeatherForecast> Get()
+            {
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+
+        }
+       
+    }
+}
